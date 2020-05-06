605350189cef76762210aa290e59249915ed5baf
carlynicholson <carlypnicholson@gmail.com>
2020-05-06 14:21:39 -0500

    added genre choices field

--- biblio/apps/api/models.py
+++ biblio/apps/api/models.py
@@ -13,7 +13,6 @@ class Book(models.Model):
 
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    # genre = models.CharField(max_length=100, blank=True, default='')
 
     FICTION = 'FIC'
     NONFICTION = 'NON'
